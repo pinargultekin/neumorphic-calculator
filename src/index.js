c8c5e3fdebbc10eb02b9b7870e6b1001dc9e302b
Pinar <pinargultekin@yahoo.com>
2020-08-02 18:14:16 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom";
+
+import "./styles.scss";
+
+const App = () => (
+  <div></div>
+);
+
+render(<App />, document.getElementById("app"));
