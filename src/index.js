606711236c0622187f1680b6916b5a716b07f666
Pinar <pinargultekin@yahoo.com>
2020-08-02 18:27:32 -0400

    Structure created

--- src/index.js
+++ src/index.js
@@ -4,7 +4,34 @@ import { render } from "react-dom";
 import "./styles.scss";
 
 const App = () => (
-  <div></div>
+  <div className= "Calculator-main">
+    <div className="Calculator-screen">
+
+    </div>
+
+    <div className="Calculator-buttons">
+    <button>%</button>
+    <button>^</button>
+    <button>√</button>
+    <button>C</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>÷</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>x</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>-</button>
+    <button>0</button>
+    <button>.</button>
+    <button>=</button>
+    <button>+</button>
+    </div>
+  </div>
 );
 
 render(<App />, document.getElementById("app"));
