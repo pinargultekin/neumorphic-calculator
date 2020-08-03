f756b96a0af945f0ae9755e3af46a974682ab61d
Pinar <pinargultekin@yahoo.com>
2020-08-03 07:52:50 -0400

    color and shadows added

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { render } from "react-dom";
 import "./styles.scss";
 
 const App = () => (
-
-
   <div className="Calculator-main">
     <form>
       <div className="Calculator-screen">
@@ -13,34 +11,119 @@ const App = () => (
       </div>
       <div className="Calculator-buttons">
         <div className="row">
-          <input className="button" type="button" name="percent" value="%" />
-          <input className="button" type="button" name="caret" value="^" />
-          <input className="button" type="button" name="square-root" value="√" />
-          <input className="button" type="button" name="clear" value="C" />
+          <input
+            className="button operation"
+            type="button"
+            name="percent"
+            value="%"
+          />
+          <input
+            className="button operation"
+            type="button"
+            name="caret"
+            value="^"
+          />
+          <input
+            className="button operation"
+            type="button"
+            name="square-root"
+            value="√"
+          />
+          <input
+            className="button clear"
+            type="button"
+            name="clear"
+            value="C"
+          />
         </div>
         <div className="row">
-          <input className="button" type="button" name="seven" value="7" />
-          <input className="button" type="button" name="eight" value="8" />
-          <input className="button" type="button" name="nine" value="9" />
-          <input className="button" type="button" name="divide" value="÷" />
+          <input
+            className="button number"
+            type="button"
+            name="seven"
+            value="7"
+          />
+          <input
+            className="button number"
+            type="button"
+            name="eight"
+            value="8"
+          />
+          <input
+            className="button number"
+            type="button"
+            name="nine"
+            value="9"
+          />
+          <input
+            className="button operation"
+            type="button"
+            name="divide"
+            value="÷"
+          />
         </div>
         <div className="row">
-          <input className="button" type="button" name="four" value="4" />
-          <input className="button" type="button" name="five" value="5" />
-          <input className="button" type="button" name="six" value="6" />
-          <input className="button" type="button" name="multiply" value="x" />
+          <input
+            className="button number"
+            type="button"
+            name="four"
+            value="4"
+          />
+          <input
+            className="button number"
+            type="button"
+            name="five"
+            value="5"
+          />
+          <input className="button number" type="button" name="six" value="6" />
+          <input
+            className="button operation"
+            type="button"
+            name="multiply"
+            value="x"
+          />
         </div>
         <div className="row">
-          <input className="button" type="button" name="one" value="1" />
-          <input className="button" type="button" name="two" value="2" />
-          <input className="button" type="button" name="three" value="3" />
-          <input className="button" type="button" name="minus" value="-" />
+          <input className="button number" type="button" name="one" value="1" />
+          <input className="button number" type="button" name="two" value="2" />
+          <input
+            className="button number"
+            type="button"
+            name="three"
+            value="3"
+          />
+          <input
+            className="button operation"
+            type="button"
+            name="minus"
+            value="-"
+          />
         </div>
         <div className="row">
-          <input className="button" type="button" name="zero" value="0" />
-          <input className="button" type="button" name="dot" value="." />
-          <input className="button" type="button" name="equal" value="=" />
-          <input className="button" type="button" name="add" value="+" />
+          <input
+            className="button number"
+            type="button"
+            name="zero"
+            value="0"
+          />
+          <input
+            className="button operation"
+            type="button"
+            name="dot"
+            value="."
+          />
+          <input
+            className="button operation"
+            type="button"
+            name="equal"
+            value="="
+          />
+          <input
+            className="button operation"
+            type="button"
+            name="add"
+            value="+"
+          />
         </div>
       </div>
     </form>
